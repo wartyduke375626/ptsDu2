ca35d314d8ac8dd1a981c78342d38f2b2c1bc7c8
Dennis Vita <vita3@uniba.sk>
2021-12-04 10:59:00 +0100

    Database change
    Changed Database class to use PraparedStatment instead of Statement and
    turned off autoCommit in querries. Now endSession method will commit
    before closing connection.

--- src/main/database/Database.java
+++ src/main/database/Database.java
@@ -21,12 +21,14 @@ public class Database implements DatabaseInterface {
     public void startSession() throws SQLException {
         if (connection != null) throw new IllegalStateException("Previous session has not been ended yet.");
         connection = DriverManager.getConnection(databaseUrl);
+        connection.setAutoCommit(false);
         System.out.println("Connection to SQLite has been established.");
     }
 
     @Override
     public void endSession() throws SQLException {
         if (connection == null) throw new IllegalStateException("Session has not been started.");
+        connection.commit();
         connection.close();
         System.out.println("Connection to SQLite closed correctly.");
         connection = null;
@@ -36,15 +38,18 @@ public class Database implements DatabaseInterface {
     public Optional<List<LineName>> getStopData(StopName stopName) throws SQLException {
         if (connection == null) throw new IllegalStateException("Session has not been started.");
 
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(Queries.getStopLinesQuery(stopName));
+        PreparedStatement preparedStatement = connection.prepareStatement(Queries.GET_STOP_LINES_QUERY);
+        preparedStatement.setString(1, stopName.toString());
+        ResultSet resultSet = preparedStatement.executeQuery();
 
         List<LineName> stopLines = new ArrayList<>();
         while (resultSet.next()) {
             stopLines.add(new LineName(resultSet.getString("lname")));
         }
         if (stopLines.isEmpty()) {
-            resultSet = statement.executeQuery(Queries.getStopQuery(stopName));
+            preparedStatement = connection.prepareStatement(Queries.GET_STOP_NAME_QUERY);
+            preparedStatement.setString(1, stopName.toString());
+            resultSet = preparedStatement.executeQuery();
             if (!resultSet.next()) return Optional.empty();
             return Optional.of(stopLines);
         }
@@ -56,14 +61,17 @@ public class Database implements DatabaseInterface {
     public Optional<Pair<StopName, List<Triplet<Integer, StopName, TimeDiff>>>> getLineFirstStopAndLineSegmentsData(LineName lineName) throws SQLException {
         if (connection == null) throw new IllegalStateException("Session has not been started.");
 
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(Queries.getLineFirstStopAndLidQuery(lineName));
+        PreparedStatement preparedStatement = connection.prepareStatement(Queries.GET_LINE_FIRST_STOP_AND_LID_QUERY);
+        preparedStatement.setString(1, lineName.toString());
+        ResultSet resultSet = preparedStatement.executeQuery();
         if (!resultSet.next()) return Optional.empty();
 
         StopName firstStop = new StopName(resultSet.getString("sname"));
         int lid = resultSet.getInt("lid");
 
-        resultSet = statement.executeQuery(Queries.getLineSegmentsDataQuery(lid));
+        preparedStatement = connection.prepareStatement(Queries.GET_LINE_SEGMENTS_DATA_QUERY);
+        preparedStatement.setInt(1, lid);
+        resultSet = preparedStatement.executeQuery();
 
         List<Triplet<Integer, StopName, TimeDiff>> lineSegmentsData = new ArrayList<>();
         while (resultSet.next()) {
@@ -81,8 +89,11 @@ public class Database implements DatabaseInterface {
     public Optional<Map<Time, Pair<Integer, List<Pair<Integer, Integer>>>>> getBussesAndPassengers(LineName lineName, Time time, TimeDiff maxStartTimeDifference) throws SQLException {
         if (connection == null) throw new IllegalStateException("Session has not been started.");
 
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(Queries.getBusesDataAndBidsQuery(lineName, time, maxStartTimeDifference));
+        PreparedStatement preparedStatement = connection.prepareStatement(Queries.GET_BUSES_DATA_AND_BIDS_QUERY);
+        preparedStatement.setString(1, lineName.toString());
+        preparedStatement.setLong(2, time.getTime()+maxStartTimeDifference.getTime());
+        preparedStatement.setLong(3, time.getTime()-maxStartTimeDifference.getTime());
+        ResultSet resultSet = preparedStatement.executeQuery();
 
         List<Triplet<Integer, Time, Integer>> buses = new ArrayList<>();
         while (resultSet.next()) {
@@ -95,8 +106,11 @@ public class Database implements DatabaseInterface {
 
         Map<Time, Pair<Integer, List<Pair<Integer, Integer>>>> result = new HashMap<>();
         for (Triplet<Integer, Time, Integer> busData : buses) {
+            preparedStatement = connection.prepareStatement(Queries.GET_BUS_SEGMENTS_PASSENGERS_QUERY);
+            preparedStatement.setInt(1, busData.getFirst());
+            resultSet = preparedStatement.executeQuery();
+
             List<Pair<Integer, Integer>> busSegmentsData = new ArrayList<>();
-            resultSet = statement.executeQuery(Queries.getBusSegmentsPassengersQuery(busData.getFirst()));
             while (resultSet.next()) {
                 int segmentIndex = resultSet.getInt("sIndex");
                 int passengers = resultSet.getInt("passengers");
@@ -122,11 +136,21 @@ public class Database implements DatabaseInterface {
                 int sIndex = indexAndUpdateValue.getFirst();
                 int newPassengers = indexAndUpdateValue.getSecond();
 
-                ResultSet resultSet = statement.executeQuery(Queries.getBIDAndLSIDQuery(startTime, lineName, sIndex));
+                PreparedStatement preparedStatement = connection.prepareStatement(Queries.GET_BID_AND_LSID_QUERY);
+                preparedStatement.setLong(1, startTime.getTime());
+                preparedStatement.setString(2, lineName.toString());
+                preparedStatement.setInt(3, sIndex);
+                ResultSet resultSet = preparedStatement.executeQuery();
+
                 if (!resultSet.next()) throw new IllegalArgumentException("Argument data does not match a busSegment in database.");
                 int bid = resultSet.getInt("bid");
                 int lsid = resultSet.getInt("lsid");
-                statement.executeUpdate(Queries.updateBusSegmentQuery(bid, lsid, newPassengers));
+
+                preparedStatement = connection.prepareStatement(Queries.UPDATE_BUS_SEGMENT_QUERY);
+                preparedStatement.setInt(1, newPassengers);
+                preparedStatement.setInt(2, bid);
+                preparedStatement.setInt(3, lsid);
+                preparedStatement.executeUpdate();
 
                 if (resultSet.next()) throw new SQLIntegrityConstraintViolationException("Argument data matches multiple busSegments in database.");
             }
@@ -138,6 +162,6 @@ public class Database implements DatabaseInterface {
         if (connection == null) throw new IllegalStateException("Session has not been started.");
 
         Statement statement = connection.createStatement();
-        statement.executeUpdate(Queries.resetPassengersQuery);
+        statement.executeUpdate(Queries.RESET_PASSENGERS_QUERY);
     }
 }
--- src/main/database/Queries.java
+++ src/main/database/Queries.java
@@ -1,67 +1,57 @@
 package database;
 
-import dataTypes.LineName;
-import dataTypes.StopName;
-import dataTypes.Time;
-import dataTypes.TimeDiff;
-import dataTypes.tuples.Pair;
-
-import java.util.List;
-
 public class Queries {
 
-    public static String getStopQuery(StopName stopName) {
-        return "SELECT s.sname\n" +
-                "FROM stop s\n" +
-                "WHERE s.sname = '" + stopName.toString() + "'";
-    }
+    public static String GET_STOP_NAME_QUERY = """
+            SELECT s.sname
+            FROM stop s
+            WHERE s.sname = ?
+            """;
 
-    public static String getStopLinesQuery(StopName stopName) {
-        return "SELECT l.lname\n" +
-                "FROM stop s, line l, stop_line sl\n" +
-                "WHERE s.sname = '"+ stopName.toString() +"' AND s.sid = sl.sid AND l.lid = sl.lid";
-    }
+    public static String GET_STOP_LINES_QUERY = """
+            SELECT l.lname
+            FROM stop s, line l, stop_line sl
+            WHERE s.sname = ? AND s.sid = sl.sid AND l.lid = sl.lid
+            """;
 
-    public static String getLineFirstStopAndLidQuery(LineName lineName) {
-        return "SELECT s.sname, l.lid\n" +
-                "FROM line l, stop s\n" +
-                "WHERE l.lname = '" + lineName.toString() + "' AND s.sid = l.firststop";
-    }
+    public static String GET_LINE_FIRST_STOP_AND_LID_QUERY = """
+            SELECT s.sname, l.lid
+            FROM line l, stop s
+            WHERE l.lname = ? AND s.sid = l.firststop
+            """;
 
-    public static String getLineSegmentsDataQuery(int lid) {
-        return "SELECT s.sname, ls.timeDiff, ls.sIndex\n" +
-                "FROM lineSegment ls, stop s\n" +
-                "WHERE ls.lid = " + lid + " AND s.sid = ls.nextStop";
-    }
+    public static String GET_LINE_SEGMENTS_DATA_QUERY = """
+            SELECT s.sname, ls.timeDiff, ls.sIndex
+            FROM lineSegment ls, stop s
+            WHERE ls.lid = ? AND s.sid = ls.nextStop
+            """;
 
-    public static String getBusesDataAndBidsQuery(LineName lineName, Time time, TimeDiff maxStartTimeDifference) {
-        return "SELECT b.startTime, b.capacity, b.bid\n" +
-                "FROM line l, bus b\n" +
-                "WHERE l.lname = '" + lineName.toString() + "' AND b.lid = l.lid AND " +
-                "b.startTime <= " + (time.getTime()+maxStartTimeDifference.getTime()) + " AND " +
-                "b.startTime >= " + (time.getTime()-maxStartTimeDifference.getTime());
-    }
+    public static String GET_BUSES_DATA_AND_BIDS_QUERY = """
+            SELECT b.startTime, b.capacity, b.bid
+            FROM line l, bus b
+            WHERE l.lname = ? AND b.lid = l.lid AND b.startTime <= ? AND b.startTime >= ?
+            """;
 
-    public static String getBusSegmentsPassengersQuery(int bid) {
-        return "SELECT bs.passengers, ls.sIndex\n" +
-                "FROM busSegment bs, lineSegment ls\n" +
-                "WHERE bs.bid = " + bid + " AND ls.lsid = bs.lsid";
-    }
+    public static String GET_BUS_SEGMENTS_PASSENGERS_QUERY = """
+            SELECT bs.passengers, ls.sIndex
+            FROM busSegment bs, lineSegment ls
+            WHERE bs.bid = ? AND ls.lsid = bs.lsid
+            """;
 
-    public static String getBIDAndLSIDQuery(Time startingTime, LineName lineName, int segmentIndex) {
-        return "SELECT b.bid, ls.lsid\n" +
-                "FROM lineSegment ls, line l, bus b\n" +
-                "WHERE l.lid = ls.lid AND b.lid = l.lid AND " +
-                "b.startTime = " + startingTime.toString() + " AND " +
-                "l.lname = '" + lineName.toString() + "' AND " +
-                "ls.sIndex = " + segmentIndex;
-    }
+    public static String GET_BID_AND_LSID_QUERY = """
+            SELECT b.bid, ls.lsid
+            FROM lineSegment ls, line l, bus b
+            WHERE l.lid = ls.lid AND b.lid = l.lid AND b.startTime = ? AND l.lname = ? AND ls.sIndex = ?
+            """;
 
-    public static String updateBusSegmentQuery(int bid, int lsid, int newPassengers) {
-        return "UPDATE busSegment\n" +
-                "SET passengers = " + newPassengers + "\n" +
-                "WHERE bid = " + bid + " AND lsid = " + lsid;
-    }
+    public static String UPDATE_BUS_SEGMENT_QUERY = """
+            UPDATE busSegment
+            SET passengers = ?
+            WHERE bid = ? AND lsid = ?
+            """;
 
-    public static String resetPassengersQuery = "UPDATE busSegment\n SET passengers = 0";
+    public static String RESET_PASSENGERS_QUERY = """
+            UPDATE busSegment
+            SET passengers = 0
+            """;
 }
